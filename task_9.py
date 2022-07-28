c2122b454b194c88d3ac3eb13b37eab8b998d374
giribabu <102803078+giribabu22@users.noreply.github.com>
2022-07-28 22:19:13 +0530

    Create task_9.py

--- task_9.py
+++ task_9.py
@@ -1,86 +1,63 @@
 from os import listdir
-from random import randrange
-import  requests,json,time
-from bs4 import BeautifulSoup as soupUse 
+import requests,json
+from bs4 import BeautifulSoup
 
-oldlist,c,l,url=[],0,[],'https://www.imdb.com/chart/top/'
-print('loaging....')
-data_list_123=listdir('data')
-p = requests.get(url)
-s = soupUse(p.content,'lxml')
+pn = '/search?q=smartphone&as=on&as-show=on&otracker=AS_Query_OrganicAutoSuggest_3_5_na_na_na&otracker1=AS_Query_OrganicAutoSuggest_3_5_na_na_na&as-pos=3&as-type=RECENT&suggestionId=smartphone&requestId=892c2da5-bc52-4313-9f31-86ddbc7fb84b&as-searchtext=smart'
+print('loading......')
+link = '/search?q=smartphone&as=on&as-show=on&otracker=AS_Query_OrganicAutoSuggest_3_5_na_na_na&otracker1=AS_Query_OrganicAutoSuggest_3_5_na_na_na&as-pos=3&as-type=RECENT&suggestionId=smartphone&requestId=892c2da5-bc52-4313-9f31-86ddbc7fb84b&as-searchtext=7&page='
+link_,un,main_li= [],1,[]
 
+for c in range(1,11):
+  ml = link+str(c)
+  link_.append(ml)
 
-def scrape_movie_details(link):
-  global c
-  zm2 = link[-10:-1:1]+'.json'
-  c+=1
-  movie={}
-  if zm2 not in data_list_123:
-    print('loading.......')
-    p=randrange(1,6)
-    time.sleep(randrange(p))
-    page=requests.get(link)
-    soup=soupUse(page.content,'lxml')
-    movie['title'] = soup.title.text
-    movie['link'] = link
-    bio = soup.find('div' ,class_ = "sc-16ede01-9 bbiYSi sc-910a7330-11 GYbFb").text
-    rating = soup.find('div', class_ = "sc-7ab21ed2-2 kYEdvH").text
-    director = soup.find('a', class_ = "ipc-metadata-list-item__list-content-item ipc-metadata-list-item__list-content-item--link").text
-    storyline = soup.find('div',class_ = "ipc-html-content-inner-div").text
-    release_data = soup.find('li', class_ = "ipc-inline-list__item").text[0:4]
-    ad=soup.find('span', class_ = "ipc-metadata-list-item__list-content-item").text  
-    more=soup.find_all('section',class_ = "ipc-page-section ipc-page-section--base celwidget" )
-    cny = soup.find('li',attrs = {'data-testid' : "title-details-origin"}).text
-    lng = soup.find('li',attrs = {'data-testid' : "title-details-languages"}).text
-    pag = soup.find_all('a')
+data = requests.get(f'https://www.flipkart.com{pn}').text
+soup = BeautifulSoup(data,'lxml')
 
-    str_Genres=' '
-    for i in pag:
-      if 'Action' == i.text or 'Drama' == i.text or 'Crime' == i.text:str_Genres = str_Genres+ i.text
-    ee = lng.find('es')
-    if ee > 1:res2 = ee+1
-    else:
-      res2=lng.index('e')
-    fl = soup.find('li',attrs={'data-testid':"title-details-companies"}).text
-    rt = soup.find('li', attrs={'data-testid':"title-techspec_runtime"}).text
-    newstring = ''.join([i for i in rt if not i.isalpha()])
-    s = int(newstring[0])*60
-    re = int(newstring[1:])+int(s)
-    e = fl.find('companies')
-    if e > 4:
-      res = e
-    else:
-      res = fl.find('company')
+def next_link():
+  global soup,pn,un
+  pn=link_[un]
+  print('\n \t loading.........')
+  un+=1
+  call_url()
 
-    movie['position'] = c
-    movie['bio'] = bio
-    movie['rating'] = rating
-    movie['director'] = director
-    movie['storyline'] = storyline
-    movie['release_date'] = release_data
-    movie['awards'] = ad
-    movie['Country of origin'] = cny[17:]
-    movie['Language'] = lng[res2+1:]
-    movie['Production company'] = fl[res:]
-    movie['runtime'] = str(re)+' '+'min'
-    movie['Genres'] = str_Genres
-    movie['Certificate'] = pag[51].text
-    zm = link[-10:-1:1]
-
-    print('this is from the database')
-
-    with open(f'data/{zm}.json','w') as file:json.dump(movie,file,indent = 3)
+def call_url():
+  global main_li
+  dl = listdir()
+  if 'data_flipkart.json' in dl:
+      with open('data_flipkart.json') as f :
+        main_li = json.load(f)
   else:
-    with open(f'data/{zm2}') as file:
-      print(json.load(file))
-
-
-def scrape_top_list():
-  li = [em  for em  in  s.find_all('td', class_ = "titleColumn")]
-  c = 1
-  for ele in li:
-    c += 1
-    lin = 'https://www.imdb.com' + ele.find('a').attrs['href']
-    scrape_movie_details(lin)
-scrape_top_list()
-
+    with open('data_flipkart.json','w') as f :
+      json.dump(main_li,f,indent=3)
+  lo = ['ROM','Display','Camera','Battery','Processor','more']
+  data = requests.get(f'https://www.flipkart.com{pn}').text
+  soup = BeautifulSoup(data,'lxml')
+  r = soup.find_all('div',class_="_30jeq3 _1_WHN1")
+  rev = soup.find_all('div',class_="_3LWZlK")
+  lo ,pp= ['ROM','Display','Camera','Battery','Processor','more'],0
+  
+  for l in soup.find_all('div',class_ ='_3pLy-c row'):
+    tit = l.find('div',class_="_4rR01T")
+    c,d=0,{}
+    hol = l.ul
+    lz = str(hol).split('>')
+    res2=rev[pp].text
+    res=r[pp].text
+    d['Name']=tit.text
+    d['prise'] = res[1:]
+    d['Rating'] = res2
+    pp+=1
+    for i in lz:
+      rem = i.split('<')
+      if c < len(lo):
+        if 'class' not in i or '' not in i :
+          d[lo[c]] = rem[0]
+          c+=1
+    main_li.append(d)
+  with open('data_flipkart.json','w') as file:
+    json.dump(main_li,file,indent=4)
+  c = input('if you want next page scraping data y/n: ')
+  if c =='y':
+    next_link()
+call_url()
